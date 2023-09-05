841244f12c22df936e91f729ba72bffd3defb2c0
viveksharma2508 <116938305+viveksharma2525@users.noreply.github.com>
2023-09-05 19:51:10 +0530

    Added the first piece of code

---
+++ migrations/20240227171706-add-password-to-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addColumn('users', 'password', {
+      type: Sequelize.STRING, // Adjust the data type according to your requirements
+      allowNull: false,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('users', 'password');
+  }
+};
